72c1e0d6daf27de89780ef046e0f582a9ce515ce
sinsys <nicholas.hazel@gmail.com>
2020-06-17 18:21:45 -0700

    Basic product and user generation completed. Models for Product and User created.

---
+++ src/mock-data/products.tsx
@@ -0,0 +1,26 @@
+// Creates fake data for use on this demo eCommerce site
+// It is intended to demonstrate scalability
+import faker from 'faker';
+import { Product } from './types.d';
+
+// Generates data about products
+const ProductsData = {
+
+  // Generates a set number of random products
+  generateProducts: (count: number): Array<Product> => {
+    const mockProducts = [];
+    for ( let i=0; i<count; i++) {
+      const mockProduct:Product = {
+        id: faker.random.uuid(),
+        name: faker.random.words(4),
+        price: faker.random.number({min: 1, max: 2000, precision: 0.01}),
+        details: faker.lorem.paragraph(4)
+      };
+      mockProducts.push(mockProduct);
+    }
+    return mockProducts;
+  }
+  
+}
+
+export default ProductsData;
---
+++ src/mock-data/users.tsx
@@ -0,0 +1,26 @@
+// Creates fake data for use on this demo eCommerce site
+// It is intended to demonstrate scalability
+import faker from 'faker';
+import { User } from './types.d';
+
+// Generates data about users
+const UsersData = {
+
+  // Generates a set number of random products
+  generateUsers: (count: number): Array<User> => {
+    const mockUsers = [];
+    for ( let i=0; i<count; i++) {
+      const mockUser: User = {
+        id: faker.random.uuid(),
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        hasCoupon: false,
+        purchases: []
+      };
+      mockUsers.push(mockUser);
+    }
+    return mockUsers;
+  }
+  
+}
+
+export default UsersData;
