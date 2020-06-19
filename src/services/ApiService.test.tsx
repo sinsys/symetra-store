2457f7aa7038aff53fd3f8658a927898e22914fc
sinsys <nicholas.hazel@gmail.com>
2020-06-19 12:02:55 -0700

    Removed comments from test file

--- src/services/ApiService.test.tsx
+++ src/services/ApiService.test.tsx
@@ -1,27 +1,5 @@
 import ApiService from './ApiService';
-  // Test Users Generation
-  // it(`User generation generates 10 users`, () => {
-  //   const users = UsersData.generateUsers(10);
-  //   expect(users).toHaveLength(10);
-  //   users.forEach(user => {
-  //     expect(typeof user.name).toBe('string');
-  //     expect(typeof user.id).toBe('string');
-  //     expect(typeof user.purchases).toBe('object');
-  //     expect(user.hasCoupon).toBeFalsy();
-  //   })
-  // });
 
-  // // Test Products Generation
-  // it(`Product generation generates 10 products`, () => {
-  //   const products = ProductsData.generateProducts(10);
-  //   expect(products).toHaveLength(10);
-  //   products.forEach(product => {
-  //     expect(typeof product.name).toBe('string');
-  //     expect(typeof product.id).toBe('string');
-  //     expect(typeof product.price).toBe('number');
-  //     expect(typeof product.details).toBe('string');
-  //   })
-  // });
 describe(`ApiService Tests`, () => {
   it(`getProducts returns n products`, () => {
     let n = 20;
